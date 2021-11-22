ffcf30af62cc2e78b0d37b52e70c14f680a14c6b
saiprashanth14 <akuthotaprashanth2003@gmail.com>
2021-11-22 07:22:10 +0530

    everything updated

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, request
-from pycaret.classification import *
 import pickle
 import numpy as np
 
 
 
-model = load_model('tuned_model')
-cols = ['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal']
+model = pickle.load(open('model.pkl', 'rb'))
 
 app = Flask(__name__)
+
 
 
 @app.route('/')
@@ -16,14 +15,22 @@ def man():
     return render_template('homepage.html')
 
 
+
 @app.route('/templates/home.html')
 def nav():
     return render_template('home.html')
 
 
+
+
 @app.route('/templates/result.html')
 def res():
     return render_template('result.html')
+
+
+
+
+
 
 
 @app.route('/predict', methods=['POST'])
@@ -42,13 +49,32 @@ def home():
     data11= int(request.form['slope'])
     data12= int(request.form['vessels'])
     data13= int(request.form['thal'])
-    arr = np.array([data1, data2, data3, data4,data5, data6, data7, data8,data9, data10, data11, data12,data13])
-    data_unseen = pd.DataFrame([arr], columns=cols)
-    prediction = predict_model(model, data=data_unseen, round=0)
-    pred = int(prediction.Label[0])
-  
+
+
+
+    arr = np.array([[data1, data2, data3, data4,data5, data6, data7, data8,data9, data10, data11, data12,data13]])
+    pred = model.predict(arr)
+
+   
     return render_template('result.html', data=pred,name=peru)
     
 
+ 
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
